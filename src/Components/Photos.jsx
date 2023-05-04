22f855851716c73ee632f9ab9024d0bd02ab71b5
M Wong <wongmt15@gmail.com>
2023-05-05 01:18:17 +1000

    5-5-23

--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -1,32 +1,30 @@
 import utsbldg5 from './images/uts-bldg5r.jpg';
 import hps from './images/hps1.jpg';
 import acupy from './images/acupy.jpg';
-
 import { useState } from 'react';
 
 const images = [acupy, utsbldg5, hps] 
 const desc = [
-				'Australian Catholic University, Strathfield campus, Sydney, Australia',
-				'UTS Haymarket Building 5, University of Technology Sydney, Australia',
-                'Homebush Public School, Homebush, NSW, Australia'
-			 ]
+   'Australian Catholic University, Strathfield campus, Sydney, Australia',
+   'UTS Haymarket Building 5, University of Technology Sydney, Australia',
+   'Homebush Public School, Homebush, NSW, Australia'
+]
 
 export default function Photos() {
-  const [count, setCount] = useState(0);
+   const [count, setCount] = useState(0);
   
-  function handleClick() {
-    setCount((count + 1)%3); 
-  }	   
-  return (
-    <div className="Photo">
-	<h2>Photos</h2>
-	<button onClick={handleClick}>
-    Next photo
-    </button>
-	<h4>{desc[count]}</h4>
-	<img src={images[count]} alt="acupy" />
-	<br />
-    </div>
-  );
+   function handleClick() {
+      setCount((count + 1) % 3); 
+   }	   
+   return (
+      <div className="Photo">
+      <h2>Photos</h2>
+      <button onClick={handleClick}>
+      Next photo
+      </button>
+      <h4>{desc[count]}</h4>
+      <img src={images[count]} alt="acupy" />
+      <br />
+      </div>
+   );
 }
-	
