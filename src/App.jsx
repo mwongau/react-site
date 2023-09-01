0129bf2e0eeade25d3c3db45e0545a70f38b1ddf
M Wong <wongmt15@gmail.com>
2023-09-02 00:59:06 +1000

    2-9-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './css/links.css';
-
 import Header from './Components/Header';
 import CV from './Components/CV';
 import Privacy from './Components/Privacy';
@@ -8,21 +7,22 @@ import Home from './Components/Home';
 import Photos from './Components/Photos';
 import { Link, Route, Routes } from "react-router-dom";
 
-function App() {
+export default function App() {
     return (	
 	<div>
 	<Header name="M Wong" sub_title="My personal site" />	
 	<nav className="Links">
-	<Link to="/">HOME</Link> | <Link to="resume">CV</Link> | <Link to="photos">PHOTOS</Link> | 
-	<Link to="privacy"> PRIVACY</Link>
+	   <Link to="/"> HOME </Link> | 
+	   <Link to="resume"> CV </Link> | 
+	   <Link to="photos"> PHOTOS </Link> | 
+	   <Link to="privacy"> PRIVACY</Link>
 	</nav> 		
 	<Routes>
-    	<Route path="/" element={<Home />} />
-    	<Route path="resume" element={<CV />} />   	
-	<Route path="photos" element={<Photos />} />
-	<Route path="privacy" element={<Privacy />} />
+       <Route path="/" element={<Home />} />
+       <Route path="resume" element={<CV />} />   	
+	   <Route path="photos" element={<Photos />} />
+	   <Route path="privacy" element={<Privacy />} />
 	</Routes>
     </div>
     );
 }
-export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,27 @@
 import "../css/notice.css"
+import { useState } from 'react';
 
-function Home() {
+import Cookies from 'js-cookie';
+
+export default function Home() {
+  const [text, setText] = useState(true);
+  function handleClick(){
+    //document.cookie = "cookiesButtonClicked=clicked"; 
+	Cookies.set('cookiesButtonClicked', 'clicked', { expires: 1 });
+	setText(false);
+  }	
+  
+  
+  const value = Cookies.get('cookiesButtonClicked');
+  let mystyle;
+  if (value === "clicked")
+    mystyle = {display:"none"};
+  else {
+    mystyle = {display:"inline"};
+  }
+  
   return (
 	<div className="Home">
-			
 		<h2>Home</h2>		
 		<div className="Intro">
 		<p>Welcome to my personal website. I am a researcher in computer vision & machine
@@ -11,10 +29,10 @@ function Home() {
 		I am also interested in web app development. This site is built by using React, Vite, 
 		Javascript, HTML and CSS. This site is hosted by GitHub Pages.</p>					
 		</div>
-		<p className="Notice">This site might use cookies in order to improve the web 
-		browsing experience of users. If you continue to use this site, it will be 
-		assumed you agree to the use of cookies.</p>
+		
+		<p class="Notice" style={mystyle}> This site uses cookies to improve your browsing experience. If you
+        continue to use this site, it will be assumed you agree to the use of cookies. 
+		<button onClick={handleClick}>OK</button></p>
     </div>	
   );
 }
-export default Home;
